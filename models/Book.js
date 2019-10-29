acedacb8a311a0a942ccfbe0c1549f134877f773
Julian <cusco2891@gmail.com>
2019-10-29 15:35:51 -0400

    Book.js schema pseudocode

---
+++ models/Book.js
@@ -0,0 +1,10 @@
+// title - Title of the book from the Google Books API
+
+// authors - The books's author(s) as returned from the Google Books API
+
+// description - The book's description as returned from the Google Books API
+
+// image - The Book's thumbnail image as returned from the Google Books API
+
+// link - The Book's information link as returned from the Google Books API
+
