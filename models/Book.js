77ec3eb3d474ce65ce0aaabfa8f22aee2d2e18c1
Julian <cusco2891@gmail.com>
2019-11-04 16:02:48 -0500

    bookschema

--- models/Book.js
+++ models/Book.js
@@ -22,10 +22,11 @@ const bookSchema = new Schema({
     title: {type: String, required: true},
     author: {type: String, required: false},
     description: {type: String, required: false},
-    thumbnail: {type: String, required: false},
+    image: {type: String, required: false},
     link: {type: String, required: false}
 });
 
 const Book = mongoose.model("Book", bookSchema);
 
 module.exports = Book;
+//SavedBooks
