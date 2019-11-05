0cb6f54bac72db9e792cb3ef0091f8fe81a9ca53
Julian <cusco2891@gmail.com>
2019-11-04 21:12:39 -0500

    serverjs routes uncommented

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
-import NavBar from "./components/Nav";
+import Nav from "./components/Nav";
 import Jumbotron from "./components/Jumbotron";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
