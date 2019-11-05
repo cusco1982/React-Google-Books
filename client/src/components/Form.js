2824700c81e8a677cbab1b54cdc02062ca62efc2
Julian <cusco2891@gmail.com>
2019-11-04 23:23:34 -0500

    form card in container

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -2,30 +2,31 @@ import React from "react";
 
 // This file exports the Input, TextArea, and FormBtn components
 
-export function Input(props) {
+export function Form(props) {
   return (
-    <div className="card">
-    
-      <h2 className="card-header">Book Search</h2>
+    <div className="container">
+      <div className="card">
+
+        <h2 className="card-header">Book Search</h2>
 
 
-      <div className="card-body">
-        <form onSubmit={props.onClick}>
+        <div className="card-body">
+          <form onSubmit={props.onClick}>
 
 
-          <div className="form-group">
-            <h6>Book</h6>
-            <input type="text" className="form-control" value={props.value} onChange={props.onChange} placeholder="Harry Potter" required />
-          </div>
+            <div className="form-group">
+              <h6>Book</h6>
+              <input type="text" className="form-control" value={props.value} onChange={props.onChange} placeholder="Harry Potter" required />
+            </div>
 
 
-          <button className="float-right" type="submit" >Search</button>
+            <button className="float-right" type="submit" >Search</button>
 
 
-        </form>
+          </form>
+        </div>
+
       </div>
-
-
     </div>
   );
 }
