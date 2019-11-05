105a5a47a7fdf065a116ca8af048171f3eedcd68
Julian <cusco2891@gmail.com>
2019-11-04 23:55:45 -0500

    app.js should be done

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
+import React from "react";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 // import logo from "./logo.svg";
 import "./App.css";
-import Nav from "./components"
+import Nav from "./components/Nav"
 import Jumbotron from "./components/Jumbotron";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
