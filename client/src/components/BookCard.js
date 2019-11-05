dbb63dda5cd46809d144cea2c6d1b7f869da9217
Julian <cusco2891@gmail.com>
2019-11-05 00:41:15 -0500

    Bookcard template

--- client/src/components/BookCard.js
+++ client/src/components/BookCard.js
@@ -2,7 +2,16 @@ import React from "react";
 
 function BookCard(props) {
     return (
-        
+
+        <div className="card">
+            <div className="row">
+
+
+                
+
+
+            </div>
+        </div>
     );
 };
 
